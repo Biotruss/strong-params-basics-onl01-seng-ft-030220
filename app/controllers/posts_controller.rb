893cb8def59924446a0adc6837e7ae3f0813110e
Craig Christopher Messi <craig.messi@gmail.com>
2020-05-16 00:37:35 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,7 +22,7 @@ class PostsController < ApplicationController
   end
    
   def create
-    @post = Post.new(post_params(:title, :description))
+    @post = Post.new(post_params)
     @post.save
     redirect_to post_path(@post)
   end
